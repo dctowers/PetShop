dfdfe41f6bf547a0015479d2f4af66c9f984ba64
FLS08 <franciscosabato08gmail.com>
2022-02-12 15:12:10 -0300

    commit to pull

--- js/juguetes.js
+++ js/juguetes.js
@@ -28,13 +28,41 @@ function updateDisplay(buscado){
     let templateHTML = ""
     toDisplay.map(item=>{
         templateHTML += `
-        <div class="box">
-                  <img src="${item.imagen}">
-                    <div class="texto">
-                        <h2>${item.nombre}</h2>
-                        <h3>precio: $${item.precio}</h3>
-                    </div>
-              </div>
+        <div class="row">
+      <div class="example-1 card">
+        <div class="wrapper">
+          <div class="date">
+            <span class="year">Stock ${item.stock<5}</span>
+            <span class="month">Limitado:</span>
+            <span class="day">5</span>
+          </div>
+          <div class="data">
+            <div class="content">
+              <span class="precio">Precio: ${item.precio}</span>
+              <h1 class="title">
+                <a href="#">${item.nombre}</a>
+              </h1>
+              <p class="text">
+                ${item.descripcion}
+              </p>
+              <label for="show-menu" class="menu-button"><span></span></label>
+            </div>
+            <input type="checkbox" id="show-menu" />
+            <ul class="menu-content">
+              <li>
+                <a href="#" class="fa fa-bookmark-o"></a>
+              </li>
+              <li>
+                <a href="#" class="fa fa-heart-o"><span>47</span></a>
+              </li>
+              <li>
+                <a href="#" class="fa fa-comment-o"><span>8</span></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>      
         `
 
         document.querySelector("#cartas").innerHTML = templateHTML
