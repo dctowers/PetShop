b115ccc6bb0bb9dac31a71479e849c66f1d6689c
dctowers <dctorres@uc.cl>
2022-02-14 02:33:27 -0300

    cards version definitiva

--- js/juguetes.js
+++ js/juguetes.js
@@ -1,98 +1,97 @@
-let articulos = []
-let juguetes = []
-let toDisplay = []
+let articulos = [];
+let juguetes = [];
+let toDisplay = [];
 
+let buscador = document.querySelector("#buscador");
 
-let buscador = document.querySelector("#buscador")
+async function getData() {
+  await fetch("https://apipetshop.herokuapp.com/api/articulos")
+    .then((response) => response.json())
+    .then((json) => articulos.push(...json.response));
+  juguetes.push(
+    ...articulos.filter((articulos) => articulos.tipo === "Juguete")
+  );
+  // console.log(juguetes);
 
-async function getData(){ 
-    await fetch("https://apipetshop.herokuapp.com/api/articulos") 
-    .then(response => response.json())
-    .then(json => articulos.push(...json.response))
-    juguetes.push(...articulos.filter(articulos=>articulos.tipo === "Juguete"))
-    // console.log(juguetes);
-
-    updateDisplay()
-    bajoStock()
+  updateDisplay();
+  bajoStock();
 }
 
-getData()
+getData();
 
 //console.log(juguetes)
 
-function updateDisplay(buscado){
-    if(buscado == undefined){
-        toDisplay.push(...juguetes)
-    }else{  
-        toDisplay.push(...buscado)
-    }
-    let templateHTML = ""
-    toDisplay.forEach(item=>{
+function updateDisplay(buscado) {
+  if (buscado == undefined) {
+    toDisplay.push(...juguetes);
+  } else {
+    toDisplay.push(...buscado);
+  }
+  let templateHTML = "";
+  toDisplay.forEach((item) => {
+    let alerta =
+      item.stock <= 5 ? `<p class="stock ">¡Ultimas unidades!</p>` : ` `;
 
-        let alerta = item.stock <= 5 ? `<p class="text-danger">Ultimas unidades!</p>`:` `
-        
-        templateHTML += `
-<a href="./detalle.html?id=${item._id}">
-    <div class="row">
-        <div class="example-1 card">
-            <div class="wrapper">
-                    <p>${alerta}</p>
-                    <div class="imgdentro">
-                        <img src="${item.imagen}" alt="">
-                    </div>
-                    <div class="date">
-                    </div>
-                <div class="data">
-                <div class="content">    
-                        <h1 class="title">
-                            <a class="titulo" href="#">${item.nombre}</a>
-                        </h1>
-                        <span class="precio">Precio: $${item.precio}</span>
-                        <div class="botones">
-                            <button onClick="getID('${item._id}')" id="${item._id}" class="btn-carrito">Añadir al carrito</button>
-                        </div>
-                </div>
+    templateHTML += `
+      <div class="card">
+        <a href="./detalle.html?id=${item._id}">
+          ${alerta}
+          <div class="cardImg">
+            <img src="${item.imagen}" alt="" />
+          </div>
+          <div class="data">
+            <div class="content">
+              <p class="title">Antiparasitario Interno Total Full CG Perros - 15 Cc
+</p>
+              <p class="price">Precio: $${item.precio}</p>
+              <div class="botones">
+                <button
+                  onClick="getID('${item._id}')"
+                  id="${item._id}"
+                  class="btn btn-primary"
+                >
+                  Añadir al carrito
+                </button>
+              </div>
             </div>
-        </div>
-    </div>
-</a>
-        `
+          </div>
+        </a>
+      </div>
+        `;
 
-        document.querySelector("#cartas").innerHTML = templateHTML
-        
-    })
+    document.querySelector("#cartas").innerHTML = templateHTML;
+  });
 }
-var lista = JSON.parse(localStorage.getItem('carrito')) || []
-var Limpiar 
+var lista = JSON.parse(localStorage.getItem("carrito")) || [];
+var Limpiar;
 
-function getID(e){
-    console.log(e.target)
-    lista.push(e)
-    const unicoCarrito = new Set(lista) 
-    var limpiar = [...unicoCarrito]
-    
-    
-    localStorage.setItem('carrito', JSON.stringify(limpiar));
-    init()
-    
+function getID(e) {
+  console.log(e.target);
+  lista.push(e);
+  const unicoCarrito = new Set(lista);
+  var limpiar = [...unicoCarrito];
+
+  localStorage.setItem("carrito", JSON.stringify(limpiar));
+  init();
 }
 
-function search(event){
-    let data = []
-    let buscar = ""
-    buscar = event.target.value
-    console.log(buscar)
+function search(event) {
+  let data = [];
+  let buscar = "";
+  buscar = event.target.value;
+  console.log(buscar);
 
-    if(buscar == ""){
-        data.push(...juguetes)
-    }else{
-        data.push(...juguetes.filter(juguete => juguete.nombre.toLowerCase().includes(buscar.toLowerCase())))
-    }
-    console.log(data);
-    updateDisplay(data)
-
+  if (buscar == "") {
+    data.push(...juguetes);
+  } else {
+    data.push(
+      ...juguetes.filter((juguete) =>
+        juguete.nombre.toLowerCase().includes(buscar.toLowerCase())
+      )
+    );
+  }
+  console.log(data);
+  updateDisplay(data);
 }
 
-buscador.addEventListener("keyup", search)
-
-
+buscador.addEventListener("keyup", search);
